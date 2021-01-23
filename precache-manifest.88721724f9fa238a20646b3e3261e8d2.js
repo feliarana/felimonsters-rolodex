af5e0229d6f9f77d6361342813af31e5705f854a
Felipe Arana <felipearana10@hotmail.com>
2021-01-23 12:18:48 -0300

    Updates

---
+++ precache-manifest.88721724f9fa238a20646b3e3261e8d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422aa8b237f9aa5fd34cb4d49e57de11",
+    "url": "/felimonsters-rolodex/index.html"
+  },
+  {
+    "revision": "5edd79f37e35555f92a4",
+    "url": "/felimonsters-rolodex/static/css/main.bfe413c2.chunk.css"
+  },
+  {
+    "revision": "ce3025c8013acde76ea7",
+    "url": "/felimonsters-rolodex/static/js/2.699c22ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/felimonsters-rolodex/static/js/2.699c22ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5edd79f37e35555f92a4",
+    "url": "/felimonsters-rolodex/static/js/main.90f01064.chunk.js"
+  },
+  {
+    "revision": "1f63255270ef4269e40a",
+    "url": "/felimonsters-rolodex/static/js/runtime-main.0b093671.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/felimonsters-rolodex/precache-manifest.88721724f9fa238a20646b3e3261e8d2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/felimonsters-rolodex/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});

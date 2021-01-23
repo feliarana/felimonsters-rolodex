2b0f9aeea88b6e457a10b115943a52f9b7956843
Felipe Arana <felipearana10@hotmail.com>
2021-01-23 12:02:46 -0300

    Final changes, added search box component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component'
 class App extends Component {
   constructor() {
     super();
@@ -8,14 +9,19 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    fetch('http://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange(e) {
+    this.setState({ searchField: e.target.value })
+  }
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
@@ -23,9 +29,10 @@ class App extends Component {
       )
     return (
       <div className="App">
-        <input type='search'
-        placeholder='search monsters'
-        onChange={ e => this.setState({ searchField: e.target.value })}
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
